a9b18d3d2e1b87a6e37574cec3472c638c29fed1
Duong Son <duongson29111997@gmail.com>
2018-12-21 21:39:19 +0700

    active account

--- demo_app/app/models/user.rb
+++ demo_app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   attr_accessor :remember_token
   before_save {self.email = email.downcase!}
   validates :name, presence: true, length: {maximum: 50}
@@ -38,10 +41,28 @@ class User < ApplicationRecord
   end
 
   # Logs out the current user.
+  def activate
+     update_columns(activated: FILL_IN, activated_at: FILL_IN)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+
+   def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
 
