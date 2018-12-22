7d0eaed3497f348aa313c1b52a776a98afca8291
Duong Son <duongson29111997@gmail.com>
2018-12-22 22:36:01 +0700

    Completed following user

--- demo_app/db/schema.rb
+++ demo_app/db/schema.rb
@@ -10,21 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220142257) do
+ActiveRecord::Schema.define(version: 20171220160347) do
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "password_digest"
-    t.string   "gender"
-    t.date     "birthday"
-    t.boolean  "admin",             default: false
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
-    t.datetime "activated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+    create_table "relationships", force: :cascade do |t|
+        t.integer  "follower_id"
+        t.integer  "followed_id"
+        t.datetime "created_at",  null: false
+        t.datetime "updated_at",  null: false
+        t.index ["followed_id"], name: "index_relationships_on_followed_id"
+        t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+        t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    end
 
+    create_table "users", force: :cascade do |t|
+        t.string   "name"
+        t.string   "email"
+        t.datetime "created_at",                        null: false
+        t.datetime "updated_at",                        null: false
+        t.string   "password_digest"
+        t.string   "gender"
+        t.date     "birthday"
+        t.boolean  "admin",             default: false
+        t.string   "activation_digest"
+        t.boolean  "activated",         default: false
+        t.datetime "activated_at"
+        t.index ["email"], name: "index_users_on_email", unique: true
+    end
+    # Following relationships
+    users = User.all
+    user  = users.first
+    following = users[2..50]
+    followers = users[3..40]
+    following.each { |followed| user.follow(followed) }
+    followers.each { |follower| follower.follow(user) }
 end
